820f795ae27c912bb82a4b6556ad5eb504311d67
RaveeshaRamanayaka <86767879+RaveeshaRamanayaka@users.noreply.github.com>
2021-08-22 22:11:03 +0530

    Update Exercise-03(All).c

--- Tutorials/Tutorial-11/Exercise-03(All).c
+++ Tutorials/Tutorial-11/Exercise-03(All).c
@@ -1,32 +1,45 @@
 #include <stdio.h>
 
+#define SALE 4
+#define PRODUCT 5
+
 int main()
 {
-    int a[4][5]={0};
-    int s,p,d;
-    int b=1;
-    int i;
-    int j;
-
-    while(b==1){
-    printf("sales person number: ");
-    scanf("%d",&s);
-    printf("product number: ");
-    scanf("%d",&p);
-    printf("total dollar value: ");
-    scanf("%d",&d);
-    a[s-1][p-1]=d;
-
-    printf("If you want to enter more, enter 1, else 0: ");
-    scanf("%d",&b);
-    printf("\n");
-    }
-
-    for(i=0;i<4;i++){
-        for(j=0;j<5;j++)
-            printf("%d person's %d product sales %d$\n",(i+1),(j+1),a[i][j]);
-        printf("\n");
-    }
-
-    return 0;
+	float salesProduct[SALE][PRODUCT] = {0};
+	int i, j;
+	float value;
+	char choice = 'y';
+	
+	while (choice == 'y') {
+		printf("\nEnter Salesperson Number(1-4) : ");
+		scanf("%d", &i);
+		printf("Enter Product Number(1-5) : ");
+		scanf("%d", &j);
+		printf("Enter Total dollar Value : $");
+		scanf("%f", &value);
+		
+		if (i < 1 || j < 1 || i > 4 || j > 5) {
+			printf("\nInvalid input!!!\n");
+			continue;
+		}
+		
+		salesProduct[i-1][j-1] = value;
+		
+		printf("\nIf you want to enter more, type 'y' : ");
+		scanf(" %c", &choice);
+		
+		if (choice != 'y') {
+			break;
+		}
+	}
+	
+	for (i = 0; i < SALE; i++) {
+		for (j = 0; j < PRODUCT; j++) {
+			printf("%d person's %d product sales $%.2f", i+1, j+1, salesProduct[i][j]);
+			printf("\n");
+		}
+		printf("\n");
+	}
+	
+	return 0;
 }
