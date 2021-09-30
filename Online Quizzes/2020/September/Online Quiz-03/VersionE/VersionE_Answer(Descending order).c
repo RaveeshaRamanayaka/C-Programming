c8d65b6776e639fbdc5e34145eb9d99f90acbce7
RaveeshaRamanayaka <86767879+RaveeshaRamanayaka@users.noreply.github.com>
2021-10-01 01:58:34 +0530

    Update VersionE_Answer(Descending order).c

--- Online Quizzes/2020/September/Online Quiz-03/VersionE/VersionE_Answer(Descending order).c
+++ Online Quizzes/2020/September/Online Quiz-03/VersionE/VersionE_Answer(Descending order).c
@@ -1,1 +1,34 @@
+//descending check
 
+#include<stdio.h>
+
+int main (void)
+{
+	//declaring array
+	int input[6];
+	
+	//declaring variavle
+	int i;
+	
+	//taking inputs from the user
+	for(i = 0 ; i < 6 ; i++)
+	{
+		printf("Enter a Number : ");
+		scanf("%d" , &input[i]);
+	}
+	
+	//check if the input sequence in ascending
+	for(i = 1 ; i < 6 ; i++)
+	{
+		if (input[i] > input[i - 1])
+		{
+			printf("Numbers are not stored in descending order.\n");
+			return -1;
+		}
+	}
+	
+	printf("Numbers are stored in descending order.\n");
+	return 0;
+	
+	return 0;
+}
